a21d2242a0ddc55bc7c1b381657f12679e77398c
Kashan Klm <141409918+Kashhan@users.noreply.github.com>
2026-01-27 17:41:30 +0500

    Create contact_manager.py

---
+++ Week1_PythonFoundation/Task2/contact_manager.py
@@ -0,0 +1,73 @@
+FILE_NAME = "contacts.txt"
+
+def add_contact(name, phone):
+    try:
+        with open(FILE_NAME, "a") as file:
+            file.write(f"{name},{phone}\n")
+        print("Contact saved successfully")
+    except Exception as e:
+        print("Error saving contact:", e)
+
+def view_contacts():
+    try:
+        with open(FILE_NAME, "r") as file:
+            contacts = file.readlines()
+            if not contacts:
+                print("No contacts found")
+            for contact in contacts:
+                name, phone = contact.strip().split(",")
+                print(f"Name: {name}, Phone: {phone}")
+    except FileNotFoundError:
+        print("Contact file does not exist")
+
+def update_contact(name, new_phone):
+    try:
+        updated = False
+        with open(FILE_NAME, "r") as file:
+            contacts = file.readlines()
+
+        with open(FILE_NAME, "w") as file:
+            for contact in contacts:
+                old_name, phone = contact.strip().split(",")
+                if old_name == name:
+                    file.write(f"{name},{new_phone}\n")
+                    updated = True
+                else:
+                    file.write(contact)
+
+        if updated:
+            print("Contact updated")
+        else:
+            print("Contact not found")
+
+    except FileNotFoundError:
+        print("Contact file not found")
+
+
+if __name__ == "__main__":
+    while True:
+        print("\n1 Add Contact")
+        print("2 View Contacts")
+        print("3 Update Contact")
+        print("4 Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            name = input("Enter name: ")
+            phone = input("Enter phone: ")
+            add_contact(name, phone)
+
+        elif choice == "2":
+            view_contacts()
+
+        elif choice == "3":
+            name = input("Enter name to update: ")
+            phone = input("Enter new phone: ")
+            update_contact(name, phone)
+
+        elif choice == "4":
+            break
+
+        else:
+            print("Invalid choice")
