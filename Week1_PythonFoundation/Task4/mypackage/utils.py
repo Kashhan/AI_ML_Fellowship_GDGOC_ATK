f5a844d288b6e1f876071074f987431b87986ce9
Kashan Klm <141409918+Kashhan@users.noreply.github.com>
2026-01-29 23:14:44 +0500

    Update utils.py

--- Week1_PythonFoundation/Task4/mypackage/utils.py
+++ Week1_PythonFoundation/Task4/mypackage/utils.py
@@ -1,5 +1,14 @@
-def safe_int(value):
-    try:
-        return int(value)
-    except ValueError:
-        return None
+def add_numbers(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+
+def print_details(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+
+
+square = lambda x: x * x
+uppercase = lambda text: text.upper()
