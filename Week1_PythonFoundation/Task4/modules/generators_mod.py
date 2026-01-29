ea7b61cdf76355c9f2e9083cbfbad6071abeabd0
Kashan Klm <141409918+Kashhan@users.noreply.github.com>
2026-01-29 22:52:11 +0500

    Create generators_mod.py

---
+++ Week1_PythonFoundation/Task4/modules/generators_mod.py
@@ -0,0 +1,26 @@
+def fibonacci(limit):
+    a, b = 0, 1
+    count = 0
+
+    while count < limit:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+
+def custom_range(start, end, step):
+    current = start
+
+    while current < end:
+        yield current
+        current += step
+
+
+def demo_generators():
+    print("Fibonacci Generator Output")
+    for num in fibonacci(10):
+        print(num)
+
+    print("\nCustom Range Generator Output")
+    for num in custom_range(1, 15, 3):
+        print(num)
